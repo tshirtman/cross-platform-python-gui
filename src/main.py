2e01558a0e19e685534d4042e6297c5aebf8d306
Gabriel Pettier <gabriel.pettier@gmail.com>
2020-09-04 01:04:28 +0200

    Update main.py

--- src/main.py
+++ src/main.py
@@ -8,12 +8,12 @@ import kivy
 #kivy.require('1.0.6') # replace with your current kivy version !
 
 from kivy.app import App
-from kivy.uix.label import Label
+from kivy.uix.button import Button
 
 class MyApp(App):
 
     def build(self):
-        return Label(text='Hello world!')
+        return Button(text='Hello world!')
 
 
 if __name__ == '__main__':
